4cf91b1b2fc38745e6a30ef0d9ec3e188f268047
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-09-11 11:47:06 -0700

    Coded operators based on instructions

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,15 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-            int i;   
+            int i = 10;   
             float f = 2.5f;
+            f+=i;
             boolean flag = true;
+            flag = !flag;
+            i++;
+            System.out.println(i);
+            System.out.println(f);
+            System.out.println(flag);
     }
 }
 
